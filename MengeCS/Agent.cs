bd67982fa75f3ee924432cd1f051bf6eb3ff5879
Sean Curtis <Sean Curtis>
2016-12-04 08:07:59 -0800

    Add new MengeCS project
    This will use the c-api of Menge to wrap into C#.  This should be Unity
    compatible.
    
    This version has been tested with Unity; it properly drives the agents.

---
+++ MengeCS/Agent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MengeCS
+{
+    /// <summary>
+    /// Wrapper class for a Menge::Agents::BaseAgent.
+    /// </summary>
+    public class Agent
+    {
+        internal Agent()
+        {
+            _pos = new Vector2();
+            _vel = new Vector2();
+            _orient = new Vector2();
+            _class = 0;
+        }
+
+        public Vector2 Position { get { return _pos; } }
+        internal Vector2 _pos;
+
+        public Vector2 Velocity { get { return _vel; } }
+        internal Vector2 _vel;
+
+        public Vector2 Orientation { get { return _orient; } }
+        internal Vector2 _orient;
+
+        public int Class { get { return _class; } }
+        internal int _class;
+    }
+}
---
+++ MengeCS/Simulator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MengeCS
+{
+    /// <summary>
+    /// Wrapper for Menge::SimulatorBase
+    /// </summary>
+    public class Simulator
+    {   
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Simulator() {
+            _agents = new List<Agent>();
+        }
+
+        public bool Initialize(String behaviorXml, String sceneXml, String model)
+        {
+            // TODO: Initialize from scene, behavior, and model.
+            const float RADIUS = 5;
+            const int COUNT = 20;
+            const float dTheta = 2.0f * (float)Math.PI / COUNT;
+            for (int i = 0; i < COUNT; ++i)
+            {
+                float theta = dTheta * i;
+                float c = (float)Math.Cos(theta);
+                float s = (float)Math.Sin(theta);
+                Agent agt = new Agent();
+                agt._pos = new Vector2(c * RADIUS, s * RADIUS);
+                _agents.Add(agt);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// The number of agents in the simulation.
+        /// </summary>
+        public int AgentCount { get { return _agents.Count;} }
+
+        /// <summary>
+        /// Returns the ith agent.
+        /// </summary>
+        /// <param name="i">Index of the agent to retrieve.</param>
+        /// <returns>The ith agent.</returns>
+        public Agent GetAgent( int i ) {
+            return _agents[i];
+        }
+
+        /// <summary>
+        /// The simulation time step.
+        /// </summary>
+        public float TimeStep { get { return _timeStep;} set { _timeStep = value;}}
+        private float _timeStep = 0.1f;
+
+        /// <summary>
+        /// Advances the simulation by the current time step.
+        /// </summary>
+        /// <returns>True if evaluation is successful and the simulation can proceed.</returns>
+        public bool DoStep() {
+            const float THETA = (float)Math.PI / 100;
+            float c = (float)Math.Cos(THETA);
+            float s = (float)Math.Sin(THETA);
+            Vector2 newPos = new Vector2();
+            foreach (Agent agt in _agents)
+            {
+                newPos.X = agt.Position.X * c + agt.Position.Y * s;
+                newPos.Y = -agt.Position.X * s + agt.Position.Y * c;
+                agt._pos.Set(newPos);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// The agents in the simulation.
+        /// </summary>
+        private List<Agent> _agents;
+    }
+}
